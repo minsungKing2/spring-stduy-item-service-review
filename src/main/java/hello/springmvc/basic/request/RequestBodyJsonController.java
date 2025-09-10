bc4dc7622bf064dae1fdd52b151613a099cabcd7
MinSung Jung <minpro22@naver.com>
2025-09-10 17:35:40 +0900

    25.09.10 HTTP Response - 정적 리소스, 뷰 템플릿, HTTP API, Message Body 예제 추가

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,11 +42,9 @@ public class RequestBodyJsonController {
 
     /**
      * requestBodyJsonV2 - @RequestBody 문자 변환
-     * @RequestBody
-     * HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
      *
-     * @ResponseBody
-     * - 모든 메서드에 @ResponseBody 적용
+     * @RequestBody HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     * @ResponseBody - 모든 메서드에 @ResponseBody 적용
      * - 메시지 바디 정보 직접 반환(view 조회 X)
      * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
      */
@@ -55,5 +54,45 @@ public class RequestBodyJsonController {
         HelloData data = objectMapper.readValue(messageBody, HelloData.class);
         log.info("username={}, age={}", data.getUsername(), data.getAge());
         return "ok";
+    }
+
+    /**
+     * requestBodyJsonV3 - @RequestBody 객체 반환
+     *
+     * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+     * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (content-type: application/json)
+     */
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
+    /**
+     * requestBodyJsonV4 - HttpEntity
+     */
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) {
+        HelloData data = httpEntity.getBody();
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
+    /**
+     * requestBodyJsonV5
+     *
+     * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+     * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (content-type: application/json)
+     * @ResponseBody 적용
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter 적용(Accept: application/json)
+     */
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return data;
     }
 }
