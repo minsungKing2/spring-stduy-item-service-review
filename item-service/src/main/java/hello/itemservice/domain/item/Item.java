a525e2793602484212778543578b054592c9e23a
MinSung Jung <minpro22@naver.com>
2025-09-11 17:37:11 +0900

    25.09.11 Welcome Page, 상품 도메인 로직 테스트 코드 추가

---
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,22 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+
+//상품 도메인 개발. Item - 상품 객체
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
